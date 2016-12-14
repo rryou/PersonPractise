e6fa1c9ca334f12fb2bd9c81c82ef6168cd55c1e
yourongrong <yourongrong@emoney-gus.local>
2016-12-14 11:40:05 +0800

    Initial Commit

---
+++ PersonLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonLearning
+//
+//  Created by yourongrong on 2016/12/14.
+//  Copyright © 2016年 益盟技术部. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
